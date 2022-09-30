02d627ee39f054f84c37540d1bbfef529651803d
devappscl <102705512+devappscl@users.noreply.github.com>
2022-09-29 22:04:21 -0300

    Update Sucursal.cs

--- Empresa/Sucursal.cs
+++ Empresa/Sucursal.cs
@@ -10,6 +10,8 @@ namespace Empresa
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        
+        public string direccion { get; set; }
        
         public List<Departamento> departamentos;
 
